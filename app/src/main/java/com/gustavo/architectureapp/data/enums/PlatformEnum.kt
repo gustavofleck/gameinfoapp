782082276681773706677517a0890a3f668b6245
gustavocfleck <gustavocfleck@gmail.com>
2021-03-28 01:16:02 -0300

    Create enum to platforms Create tests to platforms enum

---
+++ app/src/main/java/com/gustavo/architectureapp/data/enums/PlatformEnum.kt
@@ -0,0 +1,10 @@
+package com.gustavo.architectureapp.data.enums
+
+enum class PlatformEnum(val platformId: Int) {
+    XBOX_ONE(1),
+    XBOX_SERIES(186),
+    PS4(18),
+    PS5(187),
+    NINTENDO_SWITCH(7),
+    PC(4)
+}
